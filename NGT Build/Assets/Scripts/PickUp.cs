3066167ceb9e09efd753890f2404745c45ba97ba
joeyburke811 <joeyburke811@gmail.com>
2021-10-07 13:58:33 -0400

    PickUp Code
    Pickup code done from tutorial - stopped at 5:48 mark. Will continue next week. - Joey Burke

---
+++ NGT Build/Assets/Scripts/PickUp.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour {
+
+   public Transform theDest;
+
+//Right Click create empty
+//Pull in front of character
+//Rename GameObject to destination
+//Turn off gravity in Rigidbody
+//Parent the object with object itself
+
+
+   void OnMouseDown()
+   {
+       GetComponent<Rigidbody>().useGravity = false;
+       this.transform.position = theDest.position;
+       this.transform.parent = GameObject.Find("Destination").transform;
+   }
+
+   void OnMouseUp()
+   {
+       this.transform.parent = null;
+       GetComponent<Rigidbody>().useGravity = true;
+   }
+
+}
